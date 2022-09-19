b736510eb8b35be5a30397c173d25d221c4de6d0
I-Endi <endi.isuf02@gmail.com>
2022-09-19 20:43:58 +0200

    Subtraction, Deeper implementation and testing

--- solve.py
+++ solve.py
@@ -40,41 +40,47 @@ def solve_exercise(exercise_location : str, answer_location : str):
         # Check what operation within the integer arithmetic operations we need to solve
         if exercise["operation"] == "addition":
             # Solve integer arithmetic addition exercise
-            pass
+            answer = integer_addition(exercise["x"], exercise["y"] , exercise["radix"] )
+            answer = {"answer" : answer}
         elif exercise["operation"] == "subtraction":
             # Solve integer arithmetic subtraction exercise
-            pass
+            answer = integer_subtraction(exercise["x"], exercise["y"], exercise["radix"] )
+            answer = {"answer" : answer}            
         elif exercise["operation"] == "multiplication_primary":
             # Solve integer arithmetic primary school multiplication exercise
-            pass
+            answer = integer_primary_multiplication(exercise["x"], exercise["y"] , exercise["radix"] )
+            answer = {"answer" : answer}
         elif exercise["operation"] == "multiplication_karatsuba":
             # Solve integer arithmetic karatsuba multiplication exercise
-            pass
+            answer = integer_karatsuba(exercise["x"], exercise["y"], exercise["radix"] )
+            answer = {"answer" : answer}
         elif exercise["operation"] == "extended_euclidean_algorithm" :
             # Solve integer arithmetic extended euclidian algorithm exercise
-            pass
+            a, b, gcd = integer_euclidian(exercise["x"], exercise["y"], exercise["radix"] )
+            answer = {"answer-a" : a, "answer-b" : b, "answer-gcd" : gcd}
     else: # exercise["type"] == "modular_arithmetic"
         # Check what operation within the modular arithmetic operations we need to solve
         if exercise["operation"] == "reduction":
             # Solve modular arithmetic reduction exercise
-            pass
+            answer = modular_reduction(exercise["x"], exercise["modulus"] , exercise["radix"] )
+            answer = {"answer" : answer}
         elif exercise["operation"] == "addition":
             # Solve modular arithmetic addition exercise
-            pass
+            answer = modular_addition(exercise["x"], exercise["y"], exercise["modulus"], exercise["radix"] )
+            answer = {"answer" : answer}
         elif exercise["operation"] == "subtraction":
             # Solve modular arithmetic subtraction exercise
-            pass
+            answer = modular_subtraction(exercise["x"], exercise["y"], exercise["modulus"], exercise["radix"] )
+            answer = {"answer" : answer}
         elif exercise["operation"] == "multiplication":
             # Solve modular arithmetic multiplication exercise
-            pass
+            answer = modular_multiplication(exercise["x"], exercise["y"], exercise["modulus"], exercise["radix"] )
+            answer = {"answer" : answer}
         elif exercise["operation"] == "inversion":
             # Solve modular arithmetic inversion exercise
-            pass
-        
-
-
-    answer = 1
-
+            answer = modular_inversion(exercise["x"], exercise["modulus"], exercise["radix"] )
+            answer = {"answer" : answer}
+    print(answer)
     # Open file at answer_location for writing, creating the file if it does not exist yet
     # (and overwriting it if it does already exist).
     with open(answer_location, "w") as answer_file:
@@ -117,7 +123,35 @@ def integer_addition(x: string, y: string, radix: int):
     return result
 
 def integer_subtraction(x: string, y: string, radix: int):
-    pass
+
+    x, y, negativeX, negativeY = signCheck(x, y)
+    
+    if negativeX and negativeY:
+        return integer_subtraction(y, x, radix)
+    elif negativeX:
+        return integer_addition("-" + x, "-" +  y, radix)
+    elif negativeY:
+        return integer_addition(x, y, radix)
+
+    x, y = addZero(x, y)
+
+    if not(geq(x, y)):
+        return "-" + integer_subtraction(y, x, radix)
+
+    carry = False
+    digit = 0
+    result = ""
+    for i in reversed(range(len(x))):
+        digit = int(x[i]) - int(y[i]) - carry
+        if digit < 0:
+            carry = True
+            digit = digit + radix
+        else:
+            carry = False
+        result = str(digit) + result
+
+    result = removeZero(result)
+    return result
 
 def integer_primary_multiplication(x: string, y: string ,radix: int):
     pass
@@ -132,9 +166,7 @@ def integer_euclidian(x: string, y: string, radix: int):
 
 def modular_reduction(x: string, mod: string, radix: int):
     
-    if x[0] == "-":
-        negativeX = True
-        x = x[1:]
+    x, negativeX = singleSignCheck(x)
 
     while geq(x, mod):
         x = integer_subtraction(absolute(x), mod + "0"*(len(x) - len(mod)), radix)    
@@ -211,7 +243,9 @@ def geq_absolute(x: string, y: string):
 
     else:
         for i in range(len(x)):
-            if int(x[i]) < int(y[i]):
+            if int(x[i]) > int(y[i]):
+                return True
+            elif int(x[i]) < int(y[i]): 
                 return False
         return True
 
@@ -227,6 +261,14 @@ def signCheck(x: string, y: string):
         y = y[1:]
     return x, y, negativeX, negativeY
 
+def singleSignCheck(x: string):
+    
+    negativeX = False
+    if x[0] == "-":
+        negativeX = True
+        x = x[1:]
+    return x, negativeX
+
 def addZero(x: string, y: string):
     
     length_diff = len(x) - len(y)
@@ -238,6 +280,9 @@ def addZero(x: string, y: string):
 
 def removeZero(x: string):
     
-    while x[0] == "0":
+    while x[0] == "0" and len(x) > 1:
         x = x[1:]
     return x
+    
+for i in range(8,14):
+    solve_exercise("Simple\Exercises\exercise" + str(i) + ".json", "Simple\Calculated\ answer" + str(i) + ".json")
